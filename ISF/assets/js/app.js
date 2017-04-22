8dd899ca5b61661fe513347758c0577e26a3a737
Sebastien Bruere <sebastien.bruere@epitech.eu>
2017-04-22 19:42:13 +0200

    OK

--- ISF/assets/js/app.js
+++ ISF/assets/js/app.js
@@ -1,8 +1,33 @@
+$(document).foundation();
 
 
 $(document).ready( function() {
 
-
+	$('.arrow.down').click( function() {
+		$(this).css('visibility','hidden');
+		$(this).next('.hidden').slideDown( "slow", function() {
+		});
+	});
+	$('.down.un').click( function() {
+		$('.hidden.un').slideDown( "slow", function() {
+		}).prev('.arrow.down').css('visibility','hidden');
+	});
+	$('.down.deux').click( function() {
+		$('.hidden.deux').slideDown( "slow", function() {
+		}).prev('.arrow.down').css('visibility','hidden');
+	});
+	$('.down.trois').click( function() {
+		$('.hidden.trois').slideDown( "slow", function() {
+		}).prev('.arrow.down').css('visibility','hidden');
+	});
+	$('.arrow.up').click( function() {
+		$(this).parent().slideUp( "slow", function() {
+			$(this).prev().css('visibility','visible');
+		});
+	});
+	$('#bt-scroll-header').click( function() {
+		scrollTo($('.row.video'));
+	});
 });
 $(window).resize( function() {
 
@@ -14,11 +39,11 @@ $(window).resize( function() {
 
 
 function    scrollTo(next){
-  if ($(next).length != 0)
-  {
-    $('html, body').stop().animate({
-      scrollTop: $(next).offset().top + 1
-    }, 700, 'swing');
-    return false;
-  }
+	if ($(next).length != 0)
+	{
+		$('html, body').stop().animate({
+			scrollTop: $(next).offset().top + 1
+		}, 700, 'swing');
+		return false;
+	}
 };
