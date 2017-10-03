3fe66255984c10c6cb296bae944a90e9b75a6d42
Arnaud Masselin <web@adfinitas.fr>
2017-10-03 14:42:05 +0000

    Updated 'onatousunroleajouer/theme/js/tracking.js' via CloudCannon

--- onatousunroleajouer/theme/js/tracking.js
+++ onatousunroleajouer/theme/js/tracking.js
@@ -1,46 +1,48 @@
 var trackCategory = function(category) {
-    console.log('track : ' + category);
-	woopra.track('interaction',
-            {
-                category: category,
-                action: 'clic',
-                url: document.location.href,
-                title: document.title,
-                value: ''
-            });
+    woopra.track('interaction',
+        {
+            category: category,
+            action: 'clic',
+            url: document.location.href,
+            title: document.title,
+            value: ''
+        });
 };
 
 var categoryToTrack = [
-	['WEBDOCU_LP_unroleajouer17', 'WEBDOCU', 'id'],
-	['TEASERBRUNO_LP_unroleajouer17', 'TEASERBRUNO', 'id'],
-	['TEASERPARENTS_LP_unroleajouer17', 'TEASERPARENTS', 'id'],
-	['TEASERACTEURS_LP_unroleajouer17', 'TEASERACTEURS', 'id'],
-	['DECORFB_LP_unroleajouer17', 'jeLeFais', 'id'],
-	['PARTAGEFB_LP_unroleajouer17', 'partageFb', 'class'],
-	['PARTAGETW_LP_unroleajouer17', 'PARTAGETW', ''],
-	['SUIVIFB_LP_unroleajouer17', 'SUIVIFB', ''],
-	['SUIVITW_LP_unroleajouer17', 'SUIVITW', ''],
-	['SUIVIINSTA_LP_unroleajouer17', 'SUIVIINSTA', ''],
-	['SUIVITW_YT_unroleajouer17', 'SUIVITW_YT', ''],
-	['BTNDON_LP_unroleajouer17', 'BTNDON', '']
+    ['WEBDOCU_LP_unroleajouer17', 'WEBDOCU', 'id'],
+    ['TEASERBRUNO_LP_unroleajouer17', 'TEASERBRUNO', 'id'],
+    ['TEASERPARENTS_LP_unroleajouer17', 'TEASERPARENTS', 'id'],
+    ['TEASERACTEURS_LP_unroleajouer17', 'TEASERACTEURS', 'id'],
+    ['DECORFB_LP_unroleajouer17', 'jeLeFais', 'id'],
+    ['PARTAGEFB_LP_unroleajouer17', 'partageFb', 'class'],
+    ['PARTAGETW_LP_unroleajouer17', 'partageTw', 'class'],
+    ['SUIVIFB_LP_unroleajouer17', 'suiviFb', 'id'],
+    ['SUIVITW_LP_unroleajouer17', 'suiviTw', 'id'],
+    ['SUIVIINSTA_LP_unroleajouer17', 'suiviInsta', 'id'],
+    ['SUIVIYT_LP_unroleajouer17', 'suiviYt', 'id'],
+    ['BTNDON_LP_unroleajouer17', 'btnDon', 'class']
 ];
 
 function Track(id, category, type) {
-  var element;
+    var element;
   
-  if (type === 'id')
-    element = document.getElementById(id);
-  else
-  return ;
-  //  element = document.getElementsByClassName(id);
-  if (!element)
-    return ;
-    console.log('add click event on: ' + id);
-  function click(e) {
-      e.preventDefault();
-    console.log('Tracked click on this category: ' + category);
-  }
-  element.addEventListener('click', click, false);
+    if (type === 'id')
+        element = document.getElementById(id);
+    else if (type === 'class')
+        element = document.getElementsByClassName(id);
+    if (!element)
+        return ;
+    function click() {
+        trackCategory(category);
+    }
+    if (type === 'id')
+        element.addEventListener('click', click, false);
+    else if (type === 'class') {
+        for (var i = 0; i < element.length; i++) {
+            element[i].addEventListener('click', click, false);
+        }
+    }
 }
 
 var dynamicVideoEvent = function(id) {
@@ -48,7 +50,6 @@ var dynamicVideoEvent = function(id) {
     function click(e) {
         e.preventDefault();
         var activeVideo = elem.getAttribute('data-current-video-id');
-        console.log('activeVideo : ' + activeVideo);
         var category = '';
         if (activeVideo === 'teaserActeurs') {
             category = 'TEASERACTEURS_LP_unroleajouer17';
@@ -59,23 +60,13 @@ var dynamicVideoEvent = function(id) {
         } else if (activeVideo === 'webdoc') {
             category = 'WEBDOCU_LP_unroleajouer17'
         }
-        console.log('Tracked click on this category: ' + category);
+        trackCategory(category);
     }
-    console.log('add click event on: ' + id);
     elem.addEventListener('click', click, false)
 };
 
-var classEvent = function(classe) {
-    var elem = document.getElementsByClass(classe);
-    function click() {
-        
-    }
-    
-    elem.addEventListener('click', click, false);
-}
-
 $(document).ready(function() {
-    for (var i = 0; i < categoryToTrack.length; i++){
+    for (var i = 0; i < categoryToTrack.length; i++) {
         var id = categoryToTrack[i][1];
         var category = categoryToTrack[i][0];
         var type = categoryToTrack[i][2];
@@ -83,5 +74,4 @@ $(document).ready(function() {
     };
     dynamicVideoEvent('headlines1-yt-player');
     dynamicVideoEvent('campaign-yt-player');
-    classEvent('partageFb');
 });
